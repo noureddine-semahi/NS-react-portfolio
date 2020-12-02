c9c3713c347152253c681b6852b9c5efb3f237fc
unknown <kabyldorado@hotmail.com>
2020-12-02 00:11:47 -0600

    initial upload of files to repo

---
+++ src/component/about.jsx
@@ -0,0 +1,192 @@
+import React from "react";
+import layers from "../img/layers.png";
+import cogwheel from "../img/cogwheel.png";
+import pen from "../img/pen.png";
+import browser from "../img/browser.png";
+import myImage from "../img/myImage.png";
+import PropTypes from "prop-types";
+
+export default class About extends React.Component {
+	render() {
+		return (
+			<div
+				id="about-container"
+				className="content-containers container text-center mt-5">
+				<h1 id="about" className={this.props.bounceLeft}>
+					About Me
+				</h1>
+				<div className="row mt-5">
+					<div
+						className={
+							"col-6 col-sm-6 col-md-3 " + this.props.fadeInLeft
+						}>
+						<img src={layers} alt=""/>
+						<h4>Responsive</h4>
+					</div>
+					<div
+						className={
+							"col-6 col-sm-6 col-md-3 " + this.props.fadeInLeft
+						}>
+						<img src={pen} alt=""/>
+						<h4>Design</h4>
+					</div>
+					<div
+						className={
+							"col-6 col-sm-6 col-md-3 " + this.props.fadeInRight
+						}>
+						<img src={cogwheel} alt=""/>
+						<h4>Performance</h4>
+					</div>
+					<div
+						className={
+							"col-6 col-sm-6 col-md-3 " + this.props.fadeInRight
+						}>
+						<img src={browser} alt=""/>
+						<h4>Agile</h4>
+					</div>
+				</div>
+
+				<div className="row" style={{ marginTop: 7 + "rem" }}>
+					<div className="col-12 col-lg-6 hidden">
+						<img
+							className={"img-fluid " + this.props.fadeIn}
+							src={myImage}
+							alt=""
+							style={{
+								borderRadius: 50 + "%",
+								height: 250 + "px",
+								width: 250 + "px"
+							}}
+						/>
+						<p className={"mytext pt-3 " + this.props.fadeIn}>
+							My name is Noureddine Semahi, I am 33 years old, I am a full-stack web developper and I also have a Bachelors degree in Sciences of the Language, I can write and speak French, Arabic and English very fluently.
+
+							I have so far worked with few tech companies,where I filled different key-roles that allowed me to gain a decent knowledge on various softwares.
+							I was brand ambassador and beta program partner with NEXAR INC assisting with the testing and developing the key software for a smart dashcam system. Then I have worked with Apple as content review analyst with the POI team (Points Of Interests) for Apple Maps. 
+							I have also worked with the WAYMO team on developing the self driving technology as a fleet response specialist/ data analyst.
+							I later joined the YouTube team as a Data Analyst/Content Review Specialist.
+
+							I love to transform ideas into reality using code. I
+							am passionate about using Javascript and animations
+							to create awesome user experiences.
+						</p>
+					</div>
+					<div
+						className={
+							"col-12 col-sm-12 col-md-12 col-lg-6 " +
+							this.props.tada
+						}>
+						<div className="progress mb-4">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 95 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								HTML5
+							</div>
+						</div>
+						<div className="progress mb-4">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								CSS3
+							</div>
+						</div>
+						<div className="progress mb-4">
+							<div
+								className="progress-bar fill-90-bar"
+								role="progressbar"
+								style={{ width: 90 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								Javascript
+							</div>
+						</div>
+						<div className="progress mb-4">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 70 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								MongoDB
+							</div>
+						</div>
+						<div className="progress mb-4">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 60 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								MySql
+							</div>
+						</div>
+						<div className="progress mb-4">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								ReactJS
+							</div>
+						</div>
+						<div className="progress mb-4">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 75 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								NodeJs
+							</div>
+						</div>
+						<div className="progress mb-4">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 60 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								ExpressJS
+							</div>
+						</div>
+						<div className="progress mb-4">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								VanillaJS
+							</div>
+						</div>
+						<div className="progress mb-4">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 60 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								JQuery
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+About.propTypes = {
+	bounceLeft: PropTypes.string,
+	fadeInLeft: PropTypes.string,
+	fadeInRight: PropTypes.string,
+	fadeIn: PropTypes.string,
+	tada: PropTypes.string
+};
---
+++ src/component/projects.jsx
@@ -0,0 +1,261 @@
+import React from "react";
+import Vmarine from "../img/Vmarine.jpg";
+import aguaDeLuz from "../img/aguaDeLuz.png";
+import todo from "../img/todolist.png";
+import PropTypes from "prop-types";
+
+export default class Projects extends React.Component {
+
+	render() {
+		return (
+			<React.Fragment>
+				<div id="my-projects" className="container-fluid bg-light mt-5">
+					<div className=" project-container container bg-light pb-5">
+						<h1
+							id="projects"
+							className={"text-center " + this.props.fadeInRight}>
+							Portfolio
+						</h1>
+						<div className="row my-5">
+							<div
+								className={
+									"testbg col-12 col-sm-12 col-md-4 " +
+									this.props.fadeIn
+								}>
+								<div className="overlay">
+									<div className="text">
+										<h2>The SuperHero Search Engine</h2>
+									</div>
+									<div>
+										<button
+											id="vmarine-button"
+											className="project-button"
+											onClick={() => {
+												var modalBG = document.getElementById(
+													"gallery-card"
+												);
+												var marineModal = document.getElementById(
+													"vmarine"
+												);
+												modalBG.style.display = "block";
+												marineModal.style.display =
+													"block";
+											}}>
+											Learn More
+										</button>
+									</div>
+								</div>
+							</div>
+							<div
+								className={
+									"aguabg col-12 col-sm-12 col-md-4 " +
+									this.props.fadeIn
+								}>
+								<div className="overlay">
+									<div className="text">
+										<h2>The Weather Dashboard</h2>
+									</div>
+									<div>
+										<button
+											id="agua-button"
+											className="project-button"
+											onClick={() => {
+												var modalBG = document.getElementById(
+													"gallery-card"
+												);
+												var aguaModal = document.getElementById(
+													"agualuz"
+												);
+												modalBG.style.display = "block";
+												aguaModal.style.display =
+													"block";
+											}}>
+											Learn More
+										</button>
+									</div>
+								</div>
+							</div>
+							<div
+								className={
+									"todobg col-12 col-sm-12 col-md-4 " +
+									this.props.fadeIn
+								}>
+								<div className="overlay">
+									<div className="text">
+										<h2>The Subscription Hub</h2>
+									</div>
+									<div>
+										<button
+											id="todo-button"
+											className="project-button"
+											onClick={() => {
+												var modalBG = document.getElementById(
+													"gallery-card"
+												);
+												var todoModal = document.getElementById(
+													"todolist"
+												);
+												modalBG.style.display = "block";
+												todoModal.style.display =
+													"block";
+											}}>
+											Learn More
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						
+					</div>
+				</div>
+
+				{/*hidden modals*/}
+				<div id="gallery-card">
+					<div id="vmarine" className="modal-card">
+						<div className="visual">
+							<img src={Vmarine} alt=""/>
+						</div>
+						<div className="modal-info">
+							<h2>The SuperHero Search Engine API</h2>
+							<div className="modal-description">
+								<ul>
+									<li>
+										The SuperHero Search Engine API that combines the search about SuperHero carachters and all their bio in addition to all their featuring movies.
+									</li>
+									<li>
+										Designed with HTML5, CSS3, Bootstrap, 
+										Javascript, Jquery, web APIS.
+									</li>
+								</ul>
+							</div>
+							<div className="modal-bottom">
+								<h4>Comming Soon!</h4>
+								<p
+									className="close-icon"
+									style={{
+										textAlign: "right",
+										fontWeight: 900,
+										fontSize: 2 + "rem",
+										cursor: "pointer"
+									}}
+									onClick={() => {
+										var modalBG = document.getElementById(
+											"gallery-card"
+										);
+										var marineModal = document.getElementById(
+											"vmarine"
+										);
+										modalBG.style.display = "none";
+										marineModal.style.display = "none";
+									}}>
+									&#10005;
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div id="agualuz" className="modal-card">
+						<div className="visual">
+							<img src={aguaDeLuz} alt=""/>
+						</div>
+						<div className="modal-info">
+							<h2>The Weather Dashboard</h2>
+							<div className="modal-description">
+								<ul>
+									<li>
+										The Weather Dashboard les you search for the weather in different locations, it saves the cities previously searched and lets you go back and revisit them later.
+									</li>
+									<li>
+										Developed with HTML5, CSS3, Bootstrap, Webpack, Javascriptweb APIS.
+									</li>
+								</ul>
+							</div>
+							<div className="modal-bottom">
+								<a
+									href="https://noureddine-semahi.github.io/Weather-Dashboard/"
+									target="_blank">
+									<h3>View live demo</h3>
+								</a>
+								<p
+									className="close-icon"
+									style={{
+										textAlign: "right",
+										fontWeight: 900,
+										fontSize: 2 + "rem",
+										cursor: "pointer"
+									}}
+									onClick={() => {
+										var modalBG = document.getElementById(
+											"gallery-card"
+										);
+										var aguaModal = document.getElementById(
+											"agualuz"
+										);
+										modalBG.style.display = "none";
+										aguaModal.style.display = "none";
+									}}>
+									&#10005;
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div id="todolist" className="modal-card">
+						<div className="visual">
+							<img src={todo} alt=""/>
+						</div>
+						<div className="modal-info">
+							<h2>The Subscription Hub</h2>
+							<div className="modal-description">
+								<ul>
+									<li>
+										This App lets you add, store, and track all you subscriptions which can allow you to manage them effeciently and set reminders.
+									</li>
+									<li>
+										Developed with HTML5, CSS3, Bootstrap, Javascript, Ajax, Mysql, ExpressJs, Webpack, and ReactJS.
+									</li>
+								</ul>
+							</div>
+							<div className="modal-bottom">
+								<a 	
+									href="./todoList/index.html" 
+									target="_blank">
+									<h3>View live demo</h3>
+								</a>
+								<p
+									className="close-icon"
+									style={{
+										textAlign: "right",
+										fontWeight: 900,
+										fontSize: 2 + "rem",
+										cursor: "pointer"
+									}}
+									onClick={() => {
+										var modalBG = document.getElementById(
+											"gallery-card"
+										);
+										var todoModal = document.getElementById(
+											"todolist"
+										);
+										modalBG.style.display = "none";
+										todoModal.style.display = "none";
+									}}>
+									&#10005;
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
+Projects.propTypes = {
+	bounceIn: PropTypes.string,
+	fadeInLeft: PropTypes.string,
+	fadeInRight: PropTypes.string,
+	fadeIn: PropTypes.string,
+	tada: PropTypes.string
+};
